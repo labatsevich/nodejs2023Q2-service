0fd693292dcf04791b9bd9fee7e192591be92397
yurilabatsevich <yurilabatsevich@gmail.com>
2023-07-31 21:55:34 +0300

    doc: downgrade nest/swagger to v.5.2.1, update main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,20 +3,17 @@ import { AppModule } from './app.module';
 import 'dotenv/config';
 import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
 import { join } from 'path';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { SwaggerModule } from '@nestjs/swagger';
+import { readFile } from 'fs/promises';
+import { parse } from 'yaml';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
 
-  const config = new DocumentBuilder()
-    .setExternalDoc('Home library service', join(__dirname, '../doc/api.yaml'))
-    .setVersion('1.0')
-    .build();
-
-  const doc = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('doc', app, doc);
+  const document = await readFile(join(__dirname, '../doc/api.yaml'), 'utf-8');
+  SwaggerModule.setup('doc', app, parse(document));
 
   await app.listen(process.env.PORT);
 }
