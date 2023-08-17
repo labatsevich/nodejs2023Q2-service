4e4bbec9b3359f3f476afdcaad3d2187405c0c90
yurilabatsevich <yuralabatsevich@gmail.com>
2023-08-17 10:40:25 +0300

    init: started logger&auth task

---
+++ src/logger/custom-logger.service.ts
@@ -0,0 +1,22 @@
+import { LogLevel, LoggerService } from '@nestjs/common';
+
+class CustomLoggerService implements LoggerService {
+  log(message: any, ...optionalParams: any[]) {
+    throw new Error('Method not implemented.');
+  }
+  error(message: any, ...optionalParams: any[]) {
+    throw new Error('Method not implemented.');
+  }
+  warn(message: any, ...optionalParams: any[]) {
+    throw new Error('Method not implemented.');
+  }
+  debug?(message: any, ...optionalParams: any[]) {
+    throw new Error('Method not implemented.');
+  }
+  verbose?(message: any, ...optionalParams: any[]) {
+    throw new Error('Method not implemented.');
+  }
+  setLogLevels?(levels: LogLevel[]) {
+    throw new Error('Method not implemented.');
+  }
+}
