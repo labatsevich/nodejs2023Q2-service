019888db4f0bf6538759aa50247e4d019cfd6813
yurilabatsevich <yuralabatsevich@gmail.com>
2023-08-18 13:35:01 +0300

    feat: added logger middleware

---
+++ src/common/middleware/logger.middleware.ts
@@ -0,0 +1,31 @@
+import { Injectable, NestMiddleware } from '@nestjs/common';
+import { NextFunction, Request, Response } from 'express';
+import { CustomLogger } from '../../logger/custom-logger.service';
+
+@Injectable()
+export class LoggerMiddleware implements NestMiddleware {
+  constructor(private readonly logger: CustomLogger) {}
+  use(req: Request, res: Response, next: NextFunction) {
+    const { baseUrl, method, query, body } = req;
+
+    res.on('finish', () => {
+      const { statusCode } = res;
+
+      const message = `Method: ${method}, url:${baseUrl}, query: ${JSON.stringify(
+        query,
+      )}, body: ${JSON.stringify(body)}`;
+
+      if (statusCode >= 400) {
+        return this.logger.warn(message, 'HTTP');
+      }
+
+      if (statusCode >= 500) {
+        return this.logger.error(message, 'no-trace', 'HTTP');
+      }
+
+      return this.logger.log(message);
+    });
+
+    next();
+  }
+}
